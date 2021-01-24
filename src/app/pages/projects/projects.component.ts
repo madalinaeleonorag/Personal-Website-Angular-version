a9d5201a05682513eaa5689c0e890877c9e8fee1
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2021-01-25 00:10:03 +0200

    update no more filters on projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -11,57 +11,11 @@ import { PROJECTS } from 'src/assets/data';
 export class ProjectsComponent implements OnInit {
 
   data;
-  technologies: any[] = [];
-  types: any[] = [];
-  keywordsSearch = new FormControl();
-  typesSearch = new FormControl();
-  technologySearch = new FormControl();
 
   constructor() {
     this.data = PROJECTS;
-  } 
-  
+  }
+
   ngOnInit(): void {
-    this.data.forEach(element => {
-      this.technologies.push(element.technologies);
-      this.types.push(element.type);
-    });
   }
-
-  public getFilteredData() {
-    this.technologies = [...new Set(this.technologies)];
-    this.types = [...new Set(this.types)];
-    return this.data.filter((project) => {
-      return (
-        this.matchingTitle(project.name) &&
-        this.matchingDescription(project.description)
-      );
-    });
-  }
-
-  private matchingTitle(name: string) {
-    return this.keywordsSearch.value
-      ? name.toLowerCase().includes(this.keywordsSearch.value.toLowerCase())
-      : true;
-  }
-
-  private matchingDescription(name: string) {
-    return this.keywordsSearch.value
-      ? name.toLowerCase().includes(this.keywordsSearch.value.toLowerCase())
-      : true;
-  }
-
-  // private matchingType(type: string) {
-  //   if (
-  //     this.institutionTypeSelection.length > 0 &&
-  //     this.institutionTypeSelection.includes(type.toLowerCase())
-  //   ) {
-  //     return true;
-  //   } else if (this.institutionTypeSelection.length === 0) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
 }
