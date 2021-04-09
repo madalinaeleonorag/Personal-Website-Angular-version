fbb68fb940cae8d6689ed74da30752d5ab644205
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2021-04-09 23:49:52 +0300

    changed buttons and background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SkillCardComponent } from './components/skill-card/skill-card.component';
 import { InfoCardComponent } from './components/info-card/info-card.component';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 
 @NgModule({
   declarations: [
@@ -60,6 +61,7 @@ import { InfoCardComponent } from './components/info-card/info-card.component';
     NotFoundComponent,
     SkillCardComponent,
     InfoCardComponent,
+    CustomButtonComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/custom-button/custom-button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CustomButtonComponent implements OnInit {
+
+  @Input() buttonText: string;
+  @Input() isFilledButton: Boolean;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
